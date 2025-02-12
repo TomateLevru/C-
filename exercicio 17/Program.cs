c967420e04eafef2e71994de6eac46852d235fd1
TomateLevru <podlmww@gmail.com>
2025-02-12 10:57:34 +0000

    Update Program.cs

--- exercicio 17/Program.cs
+++ exercicio 17/Program.cs
@@ -4,13 +4,14 @@ public class exercicio17
 {
     public static void Main(String[] args)
     {
-
+        // array para uma lista de filmes
         String[] listadefilmes = { "hary poter", "Zorro", "Simpsons" };
+        // segundo array para o ano dos filmes
         int[] anodofilme = { 1990, 2010, 2014 };
         Console.WriteLine("lista de filmes");
         Console.WriteLine();
 
-
+        
         for (int contador = 0; contador < anodofilme.Length; contador++)
         {
             Console.WriteLine($"{listadefilmes[contador]} - {anodofilme[contador]}");
